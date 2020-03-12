a535aa35e69e1978d318a84963ba60dbdedd2d0f
Cheeserr <Cheeserr@users.noreply.github.com>
2020-03-12 10:24:21 +0000

    First code

---
+++ firstGameProject/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    private float horizontalMove = 0f;
+    public float speed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * speed * Time.fixedDeltaTime, false, false);
+    }
+}
